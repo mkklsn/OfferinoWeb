1347e9bf76444e8d088cb0b4478387bff35bbab7
mkklsn <mikkelsen.aragon@gmail.com>
2018-06-27 21:38:21 +0100

    Added sign-in button and refactored styles

--- src/client/js/header.jsx
+++ src/client/js/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserAuth from '../js/user-auth.jsx';
 import style from '../styles/header.scss';
 
 class HeaderStrip extends React.Component {
@@ -10,6 +11,7 @@ class HeaderStrip extends React.Component {
         return(
             <div className="header-strip">
                 <div className="title">Offerino</div>
+                <UserAuth />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   devServer: {
     contentBase: 'wwwroot',
-    port: 7771
+    port: 7772
   },
   plugins: [
     new CleanWebpackPlugin(['wwwroot']),
