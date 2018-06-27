bad6e6a599d8a5a0560bbb9fb1a6c7de69db1b40
mkklsn <mikkelsen.aragon@gmail.com>
2018-06-27 23:05:10 +0100

    Added font awesome react support

--- src/client/js/main.jsx
+++ src/client/js/main.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HeaderStrip from './header.jsx';
 import Content from './content.jsx';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faUtensils, faShoppingCart, faPalette, 
+  faDumbbell, faTshirt, faQuestionCircle 
+} from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/main.scss';
+
+library.add(faUtensils, faShoppingCart, faPalette, faDumbbell, faTshirt, faQuestionCircle);
 
 class Main extends React.Component {
   constructor(props){
--- src/client/js/offer.jsx
+++ src/client/js/offer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from '../styles/offer.scss';
 
 class Offer extends React.Component {
@@ -11,6 +12,9 @@ class Offer extends React.Component {
             <div className="offer">
                 <div className="title word-wrap">{this.props.title}</div>
                 <div className="owner word-wrap">{this.props.owner}</div>
+                <div className="icons">
+                    <FontAwesomeIcon icon={this.props.faicon} />
+                </div>
             </div>
         );
     }
